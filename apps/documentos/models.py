b2911bab640fff6d5acb4ef7500f1013cf9b1dee
Leonardo Cintra <leonardo.ncintra@outlook.com>
2022-08-05 17:57:49 -0300

    add media

--- apps/documentos/models.py
+++ apps/documentos/models.py
@@ -5,6 +5,7 @@ from apps.funcionarios.models import Funcionario
 
 class Documento(models.Model):
     descricao = models.CharField(max_length=100)
+    arquivo = models.FileField(upload_to="documentos")
     pertence_a = models.ForeignKey(
         Funcionario, on_delete=models.PROTECT, help_text="Dono do documento")
 
--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -122,6 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+MEDIA_ROOT = "media"
+MEDIA_URL = "media/"
+
 STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
