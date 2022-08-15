f160e64d7e8d8beffbd56dde1dd82c2106d632a9
Leonardo Cintra <leonardo.ncintra@outlook.com>
2022-08-15 17:28:00 -0300

    add env

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -10,28 +10,30 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-from distutils.log import debug
+import os
+import environ
 from pathlib import Path
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Environments config
+env = environ.Env()
+env.read_env(env_file=os.path.join(BASE_DIR, ".env"))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-hivp3tub8wg-(182!afpo5gn(tumg39pa2v$*7pgd$wnfk#2nc"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG", False)
 
-ALLOWED_HOSTS = [
-    "54.233.169.169", # AWS
-    "177.93.131.8",   # Magalu Cloud
-    "127.0.0.1",      # Desenvolvimento
-]
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 
 # Application definition
