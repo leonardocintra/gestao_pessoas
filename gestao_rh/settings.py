79c9ba7ea19d34519ec79ce6cd95f04b404f8550
Leonardo Cintra <leonardo.ncintra@outlook.com>
2022-08-24 12:15:52 -0300

    redis pass

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -156,7 +156,7 @@ LOGOUT_REDIRECT_URL = "login"
 
 # Celery
 CELERY_RESULT_BACKEND = 'django-db'
-CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_BROKER_URL = 'redis://:gestaorh@localhost:6379'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
